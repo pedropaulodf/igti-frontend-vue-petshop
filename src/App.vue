da40ed771c79f5ea0f24e9983c3a546b9b57fccd
Pedro Paulo <pedropaulotj@gmail.com>
2020-11-05 14:51:57 -0300

    limpeza de código

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-        <h3 @click="irParaHome" style="cursor: pointer">PetshopVue</h3>
+        <h3 @click="irParaHome" style="cursor: pointer">PetShop Vue</h3>
       </div>
 
       <v-spacer></v-spacer>
